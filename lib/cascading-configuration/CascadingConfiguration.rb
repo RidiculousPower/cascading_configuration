77d7a14aec8c15e5d3bd9a2b3f6c53fc4a95f70d
Asher <asher@ridiculouspower.com>
2011-08-01 03:18:37 -0400

    internal reorganization and consolidation; integration of module-cluster

--- lib/cascading-configuration/CascadingConfiguration.rb
+++ lib/cascading-configuration/CascadingConfiguration.rb
@@ -3,28 +3,8 @@ module CascadingConfiguration
   
 	include CascadingConfiguration::Variable
 
-  ###################
-  #  self.included  #
-  ###################
-  
-  def self.included( class_or_module )
-    class_or_module.instance_eval do
-      include CascadingConfiguration::Setting
-      include CascadingConfiguration::Array
-      include CascadingConfiguration::Hash
-    end
-  end
+  include CascadingConfiguration::Setting
+  include CascadingConfiguration::Array
+  include CascadingConfiguration::Hash
 
-  ###################
-  #  self.extended  #
-  ###################
-  
-  def self.extended( class_or_module )
-    class_or_module.instance_eval do
-      extend CascadingConfiguration::Setting
-      extend CascadingConfiguration::Array
-      extend CascadingConfiguration::Hash
-    end
-  end
-    
 end
