db520f9d0cbc03884f123fde6b3179dfd181d764
Asher <asher@ridiculouspower.com>
2012-01-16 17:19:50 -0500

    reverted README.rdoc to proper version, fixed base lib file requires

--- lib/cascading-configuration.rb
+++ lib/cascading-configuration.rb
@@ -1,15 +1,10 @@
 
-require 'gem-loaded'
-
-if Gem.loaded?( 'cascading-configuration' )
-	require 'cascading-configuration-setting'
-	require 'cascading-configuration-array'
-	require 'cascading-configuration-hash'
-else
-	require_relative '../setting/lib/cascading-configuration-setting.rb'
-	require_relative '../settings-array/lib/cascading-configuration-array.rb'
-	require_relative '../settings-hash/lib/cascading-configuration-hash.rb'
-end
+require 'cascading-configuration-setting'
+require 'cascading-configuration-array'
+require 'cascading-configuration-hash'
+#require_relative '../setting/lib/cascading-configuration-setting.rb'
+#require_relative '../settings-array/lib/cascading-configuration-array.rb'
+#require_relative '../settings-hash/lib/cascading-configuration-hash.rb'
 
 module CascadingConfiguration
 end
