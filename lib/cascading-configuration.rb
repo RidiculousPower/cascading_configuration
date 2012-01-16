cc60ce537544ff373d551024e48cbb0bc9571a55
Asher <asher@ridiculouspower.com>
2012-01-16 17:10:08 -0500

    major internals overhaul complete and spec'd

--- lib/cascading-configuration.rb
+++ lib/cascading-configuration.rb
@@ -1,12 +1,14 @@
 
-if $cascading_configuration_development
-	require_relative '../setting/lib/cascading-configuration-setting.rb'
-	require_relative '../settings-array/lib/cascading-configuration-array.rb'
-	require_relative '../settings-hash/lib/cascading-configuration-hash.rb'
-else
+require 'gem-loaded'
+
+if Gem.loaded?( 'cascading-configuration' )
 	require 'cascading-configuration-setting'
 	require 'cascading-configuration-array'
 	require 'cascading-configuration-hash'
+else
+	require_relative '../setting/lib/cascading-configuration-setting.rb'
+	require_relative '../settings-array/lib/cascading-configuration-array.rb'
+	require_relative '../settings-hash/lib/cascading-configuration-hash.rb'
 end
 
 module CascadingConfiguration
