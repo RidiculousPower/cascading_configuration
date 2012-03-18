cb41ef2593b613868981c39ff63b380e5820085c
Asher <asher@ridiculouspower.com>
2012-03-18 02:21:04 -0400

    update to push script, build script

--- buildgems.sh
+++ buildgems.sh
@@ -11,6 +11,9 @@ CASCADING_CONFIGURATION_ARRAY_UNIQUE=$CASCADING_CONFIGURATION/settings-array-uni
 CASCADING_CONFIGURATION_ARRAY_SORTED=$CASCADING_CONFIGURATION/settings-array-sorted
 CASCADING_CONFIGURATION_ARRAY_SORTED_UNIQUE=$CASCADING_CONFIGURATION/settings-array-sorted-unique
 CASCADING_CONFIGURATION_HASH=$CASCADING_CONFIGURATION/settings-hash
+CASCADING_CONFIGURATION_ANCESTORS=$CASCADING_CONFIGURATION/ancestors
+CASCADING_CONFIGURATION_METHODS=$CASCADING_CONFIGURATION/methods
+CASCADING_CONFIGURATION_INHERITANCE=$CASCADING_CONFIGURATION/inheritance
 CASCADING_CONFIGURATION_VARIABLE=$CASCADING_CONFIGURATION/variable
 
 gem build cascading-configuration.gemspec
@@ -39,6 +42,18 @@ mv cascading-configuration-array-sorted-unique*.gem $GEMS/
 cd $CASCADING_CONFIGURATION_HASH
 gem build cascading-configuration-hash.gemspec
 mv cascading-configuration-hash*.gem $GEMS/
+
+cd $CASCADING_CONFIGURATION_ANCESTORS
+gem build cascading-configuration-ancestors.gemspec
+mv cascading-configuration-ancestors*.gem $GEMS/
+
+cd $CASCADING_CONFIGURATION_METHODS
+gem build cascading-configuration-methods.gemspec
+mv cascading-configuration-methods*.gem $GEMS/
+
+cd $CASCADING_CONFIGURATION_INHERITANCE
+gem build cascading-configuration-inheritance.gemspec
+mv cascading-configuration-inheritance*.gem $GEMS/
 
 cd $CASCADING_CONFIGURATION_VARIABLE
 gem build cascading-configuration-variable.gemspec
--- pushgems.sh
+++ pushgems.sh
@@ -2,14 +2,19 @@
 ROOTDIR=$PWD
 GEMS=$ROOTDIR/gems/
 
+MAJOR_VERSION=2
+
 cd $GEMS
 
-gem push cascading-configuration-setting-*.gem
-gem push cascading-configuration-array-*.gem
-gem push cascading-configuration-array-unique-*.gem
-gem push cascading-configuration-array-sorted-*.gem
-gem push cascading-configuration-array-sorted-unique-*.gem
-gem push cascading-configuration-hash-*.gem
-gem push cascading-configuration-variable-*.gem
+gem push cascading-configuration-setting-${MAJOR_VERSION}*.gem
+gem push cascading-configuration-array-${MAJOR_VERSION}*.gem
+gem push cascading-configuration-array-unique-${MAJOR_VERSION}*.gem
+gem push cascading-configuration-array-sorted-${MAJOR_VERSION}*.gem
+gem push cascading-configuration-array-sorted-unique-${MAJOR_VERSION}*.gem
+gem push cascading-configuration-hash-${MAJOR_VERSION}*.gem
+gem push cascading-configuration-ancestors-${MAJOR_VERSION}*.gem
+gem push cascading-configuration-inheritance-${MAJOR_VERSION}*.gem
+gem push cascading-configuration-methods-${MAJOR_VERSION}*.gem
+gem push cascading-configuration-variable-${MAJOR_VERSION}*.gem
 
 cd $ROOTDIR
