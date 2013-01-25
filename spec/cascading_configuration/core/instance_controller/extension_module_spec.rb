83a8348e268898ce126d0cb8b0a13d5a440a24b2
Asher Haig <asher@ridiculouspower.com>
2013-01-24 22:08:33 -0500

    extension module spec done

--- spec/cascading_configuration/core/instance_controller/extension_module_spec.rb
+++ spec/cascading_configuration/core/instance_controller/extension_module_spec.rb
@@ -2,14 +2,16 @@
 require_relative '../../../../lib/cascading_configuration.rb'
 
 describe ::CascadingConfiguration::Core::InstanceController::ExtensionModule do
-
-  before :all do
-    @instance = ::Module.new do
+  
+  let( :instance ) do
+    ::Module.new do
       def self.method_in_object_but_not_module
       end
     end
-    @instance_controller = ::CascadingConfiguration::Core::InstanceController.new( @instance )
-    @ccm = ::CascadingConfiguration::Core::InstanceController::ExtensionModule.new( @instance_controller, :some_configuration )
+  end
+  let( :instance_controller ) { ::CascadingConfiguration::Core::InstanceController.new( instance ) }
+  let( :ccm ) do
+    ::CascadingConfiguration::Core::InstanceController::ExtensionModule.new( instance_controller, :some_configuration )
   end
   
   ################
@@ -17,11 +19,11 @@ describe ::CascadingConfiguration::Core::InstanceController::ExtensionModule do
   ################
   
   it 'has an instance controller' do
-    @ccm.instance_controller.should == @instance_controller
+    ccm.instance_controller.should == instance_controller
   end
   
   it 'has a type name' do
-    @ccm.configuration_name.should == :some_configuration
+    ccm.configuration_name.should == :some_configuration
   end
   
   ##################
@@ -29,10 +31,10 @@ describe ::CascadingConfiguration::Core::InstanceController::ExtensionModule do
   ##################
   
   it 'can pend method aliases' do
-    @ccm.alias_method( :alias_name, :method_in_object_but_not_module )
-    @instance.respond_to?( :alias_name ).should == false
-    @instance.extend( @ccm )
-    @instance.respond_to?( :alias_name ).should == true
+    ccm.alias_method( :alias_name, :method_in_object_but_not_module )
+    instance.respond_to?( :alias_name ).should == false
+    instance.extend( ccm )
+    instance.respond_to?( :alias_name ).should == true
   end
 
 end
