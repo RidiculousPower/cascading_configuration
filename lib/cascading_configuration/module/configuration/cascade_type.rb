45a56a59970b94f0889a059fb7796c00c46b48bf
Asher Haig <asher@ridiculouspower.com>
2013-02-02 10:56:50 -0500

    partial cascade_type changes that probably will not be used due to complexities of inheritance

--- lib/cascading_configuration/module/configuration/cascade_type.rb
+++ lib/cascading_configuration/module/configuration/cascade_type.rb
@@ -6,23 +6,29 @@ class ::CascadingConfiguration::Module::Configuration::CascadeType
   ################
   
   ###
-  # @param [Symbol,String] type_name
+  # @overload initialize( type_name, method_type, ..., & cascade_test_block )
   #
-  #        Name of cascade type.
+  #   @param [Symbol,String] type_name
+  #   
+  #          Name of cascade type.
+  #   
+  #   @param [:singleton,:instance,:class,:module,:local_instance,:object] method_type
+  #   
+  #          Where methods should be defined for this configuration.
   #
-  # @yield cascade_test_block
+  #   @yield cascade_test_block
+  #   
+  #          Block to test whether cascade to instance should occur.
+  #   
+  #     @yieldparam [Object] instance
+  #   
+  #                 Instance to which cascade would occur.
+  #   
+  #     @yieldreturn [true,false]
+  #   
+  #                  Whether cascade should occur.        
   #
-  #        Block to test whether cascade to instance should occur.
-  #
-  #   @yieldparam [Object] instance
-  #
-  #               Instance to which cascade would occur.
-  #
-  #   @yieldreturn [true,false]
-  #
-  #                Whether cascade should occur.        
-  #
-  def initialize( type_name, & cascade_test_block )
+  def initialize( type_name, *method_types, & cascade_test_block )
     
     @name = type_name
     
