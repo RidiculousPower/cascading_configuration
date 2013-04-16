8849fd2abcc3a5989614b2ad4bf8040cd7f7fea1
Asher Haig <asher@ridiculouspower.com>
2013-04-15 23:11:37 -0400

    fixed unregister for singleton => instance relations

--- lib/cascading_configuration/controller.rb
+++ lib/cascading_configuration/controller.rb
@@ -734,21 +734,31 @@ module ::CascadingConfiguration::Controller
     case instance
       when ::Module
         if ( instance_class = instance.class ) < ::Module and not instance_class < ::Class
-          if parent_instance_configurations = instance_configurations( parent )
-            singleton_configurations( instance ).unregister_parent( parent_instance_configurations )
+          if parent_instance_configurations = instance_configurations( parent, false )
+            if singleton_configurations = singleton_configurations( instance, false )
+              singleton_configurations.unregister_parent( parent_instance_configurations )
+            end
           end
         else
-          if parent_singleton_configurations = singleton_configurations( parent )
-            singleton_configurations( instance ).unregister_parent( parent_singleton_configurations )
+          if parent_singleton_configurations = singleton_configurations( parent, false )
+            if singleton_configurations = singleton_configurations( instance, false )
+              singleton_configurations.unregister_parent( parent_singleton_configurations )
+            end
           end
-          if parent_instance_configurations = instance_configurations( parent )
-            instance_configurations( instance ).unregister_parent( parent_instance_configurations )
+          if parent_instance_configurations = instance_configurations( parent, false )
+            if instance_configurations = instance_configurations( instance, false )
+              instance_configurations.unregister_parent( parent_instance_configurations )
+            end
           end
         end
       else
-        if parent_instance_configurations = instance_configurations( parent )
-          instance_configurations( instance ).unregister_parent( parent_instance_configurations )
+        parent_configurations = case parent
+          when ::Module
+            singleton_configurations( parent, false )
+          else
+            instance_configurations( parent, false )
         end
+        instance_configurations( instance ).unregister_parent( parent_configurations ) if parent_configurations
     end
 
     return self
